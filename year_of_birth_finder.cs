fd02ba4428b17c39da955cfab4a3bbb7f42d124e
Karwan-018 <karwannazm8@gmail.com>
2021-01-14 19:05:42 +0300

    commit

---
+++ year_of_birth_finder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age ");
+            int age = Convert.ToInt32(Console.In.ReadLine());
+            int currentYear = DateTime.Now.Year;
+            int birthyear = currentYear - age;
+            Console.WriteLine("your were born in  " + birthyear);
+        }
+    }
+}
